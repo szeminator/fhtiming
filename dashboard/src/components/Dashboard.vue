7b2fc6438bc4985656d07eebde525edb6a258f84
Riss Tatjana <Tatjana.Riss@edu.fh-kaernten.ac.at>
2024-01-07 21:55:23 +0100

    added InfoBoxPercentage.vue and InfoBoxValue.vue

--- dashboard/src/components/Dashboard.vue
+++ dashboard/src/components/Dashboard.vue
@@ -1,13 +1,25 @@
 <template>
+  <div class="info-container">
+    <InfoBoxValue title="Info" />
+    <InfoBoxPercentage title="Info2" />
+  </div>
   <div>
     <h1>This is the Dashboard</h1>
   </div>
+
 </template>
 
 <script setup lang="ts">
+import InfoBoxValue from './InfoBoxValue.vue';
+import InfoBoxPercentage from './InfoBoxPercentage.vue';
+
 // Add your script here
 </script>
 
 <style scoped>
-/* Add your styles here */
+.info-container {
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px; /* Adjust the gap as needed */
+}
 </style>
---
+++ dashboard/src/components/InfoBoxPercentage.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+/* interface Props {
+  title: string,
+  content: string,
+}
+const { 
+  title, 
+  content 
+    }= defineProps<Props>() */
+
+</script>
+
+<template>
+  <div class="infobox">
+    <h3 class="text-lg font-semibold title">Info fasddf dasdfadsf sasf asdf asdf adfas   f</h3>
+    <p class="content">Info 2</p>
+  </div>
+  </template>
+  
+  <style scoped>
+  .infobox {
+    display: flex;
+    flex-direction: column;
+    width: 188px;
+    min-height: 108px; /* Minimum height */
+    padding: 3px 9px 9px 9px; /* Increased padding for the box */
+    align-items: flex-start;
+    gap: 10px;
+    border-radius: 5px;
+    border: 2px solid var(--Bright-Cape-colors-BC-Blue, #417DA8);
+    box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.30);
+  }
+  
+  .title {
+    align-self: flex-start;
+    margin: 0px;
+    text-align: left;
+  }
+  
+  .content {
+    margin-top: auto; /* Added to push content to the bottom */
+    margin-bottom: 0px;
+    align-self: flex-end;
+    color: #76C657;
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -0.14px;
+    text-align: right;
+  }
+  
+  .title, .content {
+    padding: 2px;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  </style>
+    
