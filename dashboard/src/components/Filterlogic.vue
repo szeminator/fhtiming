cdeac289be949407d25ce843f8f6c4ec035e65cf
szeminator <hannes.satz@diamir.io>
2024-02-02 01:50:48 +0100

    Remove unused import statement

--- dashboard/src/components/Filterlogic.vue
+++ dashboard/src/components/Filterlogic.vue
@@ -77,7 +77,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted, computed } from 'vue';
+import { ref, watch, onMounted } from 'vue';
 import { useStore } from '../store';
 import { useRouter } from 'vue-router';
 import {fetchStartersThatDidntGetFar, selectRunnersForSplit, filterFemales, filterMales} from '../insights';
