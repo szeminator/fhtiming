a0ea4bb5106c1563e36bc50963a122939c1206d7
szeminator <hannes.satz@diamir.io>
2024-02-04 17:24:15 +0100

    Update interval length to 2 seconds

--- dashboard/src/components/Filterlogic.vue
+++ dashboard/src/components/Filterlogic.vue
@@ -112,7 +112,7 @@ let progressIntervalId: number | null | undefined = null;
 let intervalId: number | null | undefined = null;
 const checkbox_filterFemales = ref(false);
 const checkbox_filterMales = ref(false);
-let intervalLengthInSeconds = 10;
+let intervalLengthInSeconds = 2;
 
 const keyMappings = {
   start: 'Start Number',
