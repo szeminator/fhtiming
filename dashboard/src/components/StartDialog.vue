d21ee3ce01427c0f5cf32130abcf26104c0d0d2e
szeminator <hannes.satz@diamir.io>
2024-02-04 21:36:29 +0100

    Update docker-compose.yml and StartDialog.vue

--- dashboard/src/components/StartDialog.vue
+++ dashboard/src/components/StartDialog.vue
@@ -4,7 +4,7 @@
       <p>
         Streckenauswertung bietet eine visuelle Darstellung der Zeitdaten von Läufer:innen, Fahrer:innen und Radfahrer:innen. 
         Durch Diagramme und Tabellen können Sie leicht die Leistung jedes/jeder Teilnehmers/Teilnehmerin verfolgen und vergleichen. 
-        Dies ermöglicht eine effektive Analyse und verbessert das Verständnis der Rennbedingungen.
+        Dies ermöglicht eine effektive Analyse und verbessert das Verständnis der Rennbedingungen. Und durch die flexible Splitnummer können Sie die Leistung in verschiedenen Abschnitten der Strecke vergleichen.
       </p>
     </div>
 </template>
